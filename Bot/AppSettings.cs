59b7e92c8f263542d7a9f6c920e2c358132dc04a
Júnior Nasicmento <junior.nascm@gmail.com>
2024-03-29 22:23:24 -0300

    feat: fix

--- Bot/AppSettings.cs
+++ Bot/AppSettings.cs
@@ -2,6 +2,6 @@
 
 public class AppSettings
 {
-    public string Token { get; set; } = string.Empty;
-    public string Prefix { get; set; } = string.Empty;
+    public string Token { get; init; } = string.Empty;
+    public string Prefix { get; init; } = string.Empty;
 }
