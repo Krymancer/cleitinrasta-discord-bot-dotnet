3f15c2b824d3ad45baa52269b15e46c4019e4a61
Júnior Nasicmento <junior.nascm@gmail.com>
2024-03-31 18:17:39 -0300

    feat: change bot lib

---
+++ App/Modules/SlashCommandModule.cs
@@ -0,0 +1,9 @@
+﻿using NetCord.Services.ApplicationCommands;
+
+namespace App.Modules;
+
+public class SlashCommandModule : ApplicationCommandModule<SlashCommandContext>
+{
+    [SlashCommand("pong", "Pong!")]
+    public static string Pong() => "Ping!";
+}
