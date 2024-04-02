934f1834029bae4b1b53f8a32311d08eb62a80f4
Júnior Nascimento <junior.nascm@gmail.com>
2024-04-01 23:53:08 -0300

    feat: add playlist support

--- App/Modules/SlashCommandModule.cs
+++
@@ -1,9 +0,0 @@
-﻿using NetCord.Services.ApplicationCommands;
-
-namespace App.Modules;
-
-public class SlashCommandModule : ApplicationCommandModule<SlashCommandContext>
-{
-    [SlashCommand("pong", "Pong!")]
-    public static string Pong() => "Ping!";
-}
